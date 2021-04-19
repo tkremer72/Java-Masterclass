c4bf5882c5953100f512576bbf5bbc24b7ab3f3c
Thomas Kremer <tbone7243@gmail.com>
2021-04-19 08:11:52 -0400

    Created HelloWorld project, created a main class that prints Hello Thomas to the terminal window as well as the integer total of an expression.

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class Hello {
+     public static void main(String[] args) {
+          System.out.println("Hello, Thomas!");
+
+          int myFirstNumber = (15 + 10) + (2 * 10);//Just like JS you can use expressions 
+          System.out.println(myFirstNumber);
+     }
+
+}
+
