10c30dabcccc4024e447442bf5a7d7de8743fbcf
Thomas Kremer <tbone7243@gmail.com>
2021-04-19 08:26:57 -0400

    Finished up Starting out with expressions, section 24.  Completed all of the challenges.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,8 +4,16 @@ public class Hello {
      public static void main(String[] args) {
           System.out.println("Hello, Thomas!");
 
-          int myFirstNumber = (15 + 10) + (2 * 10);//Just like JS you can use expressions 
-          System.out.println(myFirstNumber);
+          int myFirstNumber = (10 + 5) + (2 * 10);//Just like JS you can use expressions 
+          int mySecondNumber = 12;
+          int myThirdNumber = myFirstNumber * 2;//lines 9-10 are part of second challenge
+          int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+          System.out.println(myTotal);
+          int myLastOne = 1000 - myTotal;
+          System.out.println(myLastOne);
+
+
      }
 
 }
