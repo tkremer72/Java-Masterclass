fe69c349576abdb806177be4876b77324ff081af
Thomas Kremer <tbone7243@gmail.com>
2021-04-23 08:46:25 -0400

    Completed the first two coding excercises, speed converter and megaBytes converter.

--- SpeedConverter/src/SpeedConverter.java
+++
@@ -1,9 +0,0 @@
-public class SpeedConverter {
-
-    public static long toMilesPerHour(double kilometersPerHour) {
-        if(kilometersPerHour < 0) {
-            return -1;
-        }
-        return Math.round(kilometersPerHour / 1.609);
-    }
-}
