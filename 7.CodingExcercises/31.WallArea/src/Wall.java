da53bbd24e8e31ec76cb0b203cbbfb06e1df8333
Thomas Kremer <tbone7243@gmail.com>
2021-04-28 15:34:51 -0400

    Coding excercises 31-33 complete.  Started on coding excercise 34 complex operations.

--- 7.CodingExcercises/31.WallArea/src/Wall.java
+++ 7.CodingExcercises/31.WallArea/src/Wall.java
@@ -1,2 +1,44 @@
 public class Wall {
+    //two fields width and height
+    private double width;
+    private double height;
+
+    public Wall(double width, double height) {
+        if(width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+        if(height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+    public Wall() {
+        this(0, 0);
+    }
+    public double getWidth() {
+        return width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setWidth(double width) {
+        if(width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+    public void setHeight(double height) {
+        if(height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+    public double getArea() {
+        return width * height;
+    }
 }
