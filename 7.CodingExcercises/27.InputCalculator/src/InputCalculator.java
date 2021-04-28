a9c64dc254564705ccd79a1c1752914d6e24879a
Thomas Kremer <tbone7243@gmail.com>
2021-04-28 07:22:26 -0400

    Worked up to coding excercise 27, Input calculator.  I could not see any results in the console for these but the solution is correct?

---
+++ 7.CodingExcercises/27.InputCalculator/src/InputCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+        double sum = 0;
+        double average = 0;
+        int count = 0;
+
+        while(true) {
+            boolean isAnInt = scanner.hasNextInt();
+            if(isAnInt) {
+                number = scanner.nextInt();
+                sum += number;
+                count++;
+            } else {
+                break;
+            }
+        }
+        scanner.nextLine();
+        average = (sum/count);
+        System.out.println("SUM = "+ Math.round(sum) +" AVG = "+ Math.round(average) );
+        scanner.close();
+    }
+}
---
+++ 7.CodingExcercises/27.InputCalculator/src/ReadMe.md
@@ -0,0 +1,28 @@
+# Input Calculator
+
+Write a method named inputThenPrintSumAndAverage that does not have any parameters
+
+The method should not return anyting(void) and it needs to keep reading int numbers from
+they keyboard.
+
+When the user enters something that is not an int then it needs to print a message in the format 
+"SUM = XX AVG = YY".
+
+XX represents the sum of all entered numbers of type int
+YY represents the calculated average of all numbers of type long
+
+Examples of Input/Output
+Example 1:
+    Input:
+    1
+    2
+    3
+    4
+    5
+    a
+    Output:
+    SUM = 15 AVG = 3;
+Example 2:
+    Input:
+    hello
+    SUM = 0 AVG = 0;
