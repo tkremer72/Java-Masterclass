17e6b212dedebf3302c4a5ed3e05f41b68df041d
Thomas Kremer <tbone7243@gmail.com>
2021-05-03 06:52:53 -0400

    Finished first half of the OOP Main Challenge.  Started second half.

---
+++ 15.OOPMasterChallenge/src/Hamburger.java
@@ -0,0 +1,84 @@
+
+public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private String rollType;
+
+    private String addOn1Name;
+    private double addOn1Price;
+
+    private String addOn2Name;
+    private double addOn2Price;
+
+    private String addOn3Name;
+    private double addOn3Price;
+
+    private String addOn4Name;
+    private double addOn4Price;
+
+    public Hamburger(String name, String meat, double price, String rollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.rollType = rollType;
+
+    }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.addOn1Name = name;
+        this.addOn1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addOn2Name = name;
+        this.addOn2Price = price;
+    }
+    public void addHamburgerAddition3(String name, double price) {
+        this.addOn3Name = name;
+        this.addOn3Price = price;
+    }
+    public void addHamburgerAddition4(String name, double price) {
+        this.addOn4Name = name;
+        this.addOn4Price = price;
+    }
+
+    public double itemizeHamburger() {
+        double hamburgerPrice = this.price;
+        System.out.println(this.name + " hamburger " + " on a " + this.rollType + " roll "
+                    + "with " + this.meat + ", price is "  + this.price);
+        if(this.addOn1Name != null) {
+            hamburgerPrice += this.addOn1Price;
+            System.out.println("Added " + this.addOn1Name + " for an extra " + this.addOn1Price);
+        }
+        if(this.addOn2Name != null) {
+            hamburgerPrice += this.addOn2Price;
+            System.out.println("Added " + this.addOn2Price + " for an extra " + this.addOn2Price);
+        }
+        if(this.addOn3Name != null) {
+            hamburgerPrice += this.addOn3Price;
+            System.out.println("Added " + this.addOn3Name + " for an extra " + this.addOn3Price);
+        }
+        if(this.addOn4Name != null) {
+            hamburgerPrice += this.addOn4Price;
+            System.out.println("Added " + this.addOn4Name + " for an extra " + this.addOn4Price);
+        }
+
+        return hamburgerPrice;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
