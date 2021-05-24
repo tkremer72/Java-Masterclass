68b0ed9a9b7dc012cd9baeed925e934b878bdd20
Thomas Kremer <tbone7243@gmail.com>
2021-05-24 12:19:55 -0400

    Finished section on FileChannel to copy file and pipes with threads.  Starting on the Filesystem section.

--- 22.BasicInputAndOutput/10.AbsoluteAndRelativeReadsJavaNIO/src/com/webtek/java/master_class/input_output/java_nio/Main.java
+++ 22.BasicInputAndOutput/10.AbsoluteAndRelativeReadsJavaNIO/src/com/webtek/java/master_class/input_output/java_nio/Main.java
@@ -1,65 +1,74 @@
 package com.webtek.java.master_class.input_output.java_nio;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
-import java.util.Random;
+import java.nio.channels.Pipe;
+import java.nio.charset.StandardCharsets;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        //code for the writer using pipe
+        try {
+            Pipe pipe = Pipe.open();
+            Runnable writer = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Pipe.SinkChannel sinkChannel = pipe.sink();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
 
-        try (FileOutputStream binaryFile = new FileOutputStream("data.dat");
-             FileChannel binaryChannel = binaryFile.getChannel()) {
-//Create a buffer
-            ByteBuffer buffer = ByteBuffer.allocate(100);
+                        for(int i = 0; i < 10; i++) {
+                            String currentTime = "The time is: " + System.currentTimeMillis();
 
-            byte[] outputBytes = "Hello World!".getBytes();
-            byte[] outputBytes2 = "Nice to meet you".getBytes();
-            //Chaining puts together
-            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
-            buffer.flip();//resets the buffer position back to 0
+                            buffer.put(currentTime.getBytes());
+                            buffer.flip();
 
-            //write to a single buffer
-//            byte[] outputBytes = "Hello World!".getBytes();
-//            buffer.put(outputBytes);
-//            buffer.putInt(245);
-//            buffer.putInt(-98765);
-//            byte[] outputBytes2 = "Nice to meet you".getBytes();
-//            buffer.put(outputBytes2);
-//            buffer.putInt(1000);
-//            buffer.flip();//resets the buffer position back to 0
-            binaryChannel.write(buffer);
+                            while(buffer.hasRemaining()) {
+                                sinkChannel.write(buffer);
+                            }
+                            buffer.flip();
+                            Thread.sleep(100);
 
-        //Read from the single buffer
-            RandomAccessFile randomAccessFile = new RandomAccessFile("data.dat","rwd");
-            FileChannel channel = randomAccessFile.getChannel();
+                        }
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
 
-            ByteBuffer readBuffer = ByteBuffer.allocate(100);
-            channel.read(readBuffer);
-            readBuffer.flip();
-            byte[] inputString = new byte[outputBytes.length];
-            readBuffer.get(inputString);
-            System.out.println("inputString = " + new String(inputString));
-            System.out.println("int1 = " + readBuffer.getInt());
-            System.out.println("int2 = " + readBuffer.getInt());
-            byte[] inputString2 = new byte[outputBytes2.length];
-            readBuffer.get(inputString2);
-            System.out.println("inputString2 = " + new String(inputString2));
-            System.out.println("int3 = " + readBuffer.getInt());
+            //Code for the reader using pipe
 
+            Runnable reader = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Pipe.SourceChannel sourceChannel = pipe.source();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
 
-        } catch (IOException e) {
-            System.out.println("I/O Exception Thrown! " + e.getMessage());
+                        for(int i = 0; i < 10; i++) {
+                            int bytesRead = sourceChannel.read(buffer);
+                            byte[] timeString = new byte[bytesRead];
+                            buffer.flip();
+                            buffer.get(timeString);
+                            System.out.println("Reader Thread: " + new String(timeString));
+                            buffer.flip();
+                            Thread.sleep(100);
+                        }
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(writer).start();
+            new Thread(reader).start();
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
