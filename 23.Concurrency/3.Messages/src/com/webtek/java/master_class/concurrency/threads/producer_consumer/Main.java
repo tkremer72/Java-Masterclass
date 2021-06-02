04a8a9d910ec95337d6bce718b557ad7da5f3165
Thomas Kremer <tbone7243@gmail.com>
2021-06-02 08:46:25 -0400

    Finished lecture 271 - 275.Reentrant lock and unlock.  Starting lecture 276.Using Try Finally With Threads.

--- 23.Concurrency/3.Messages/src/com/webtek/java/master_class/concurrency/threads/producer_consumer/Main.java
+++ 23.Concurrency/3.Messages/src/com/webtek/java/master_class/concurrency/threads/producer_consumer/Main.java
@@ -1,9 +1,14 @@
 package com.webtek.java.master_class.concurrency.threads.producer_consumer;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        //Create and run methods
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -13,18 +18,80 @@ class Message {
 
     //Read method used by the consumer to read a message
     public synchronized String read() {
-        while(empty) {
 
+        while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException ie) {
+
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
+
     //Write method used by the producer to write a message
     public synchronized void write(String message) {
-        while(!empty) {
 
+        while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException ie) {
+
+            }
         }
         empty = false;
         this.message = message;
+        notifyAll();
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String messages[] = {
+                "Humpty Dumpty Sat On A Wall!",
+                "Humpty Dumpty Had A Great Fall!",
+                "All The Kings Horses And All The Kings Men,",
+                "Couldn't Put Humpty Together Again!"
+        };
+        Random random = new Random();
+
+        for (int i = 0; i < messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException ie) {
+
+            }
+        }
+        message.write("Finished!");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished!");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException ie) {
+
+            }
+        }
     }
 }
---
+++ 23.Concurrency/4.ProducerConsumer/src/com/webtek/java/master_class/concurrency/threads/producer_consumer/Main.java
@@ -0,0 +1,90 @@
+package com.webtek.java.master_class.concurrency.threads.producer_consumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.webtek.java.master_class.concurrency.threads.producer_consumer.Main.EOF;
+
+public class Main {
+    public static final String EOF = "EOF";
+
+    public static void main(String[] args) {
+        List<String> buffer = new ArrayList<>();
+
+        ReentrantLock bufferLock = new ReentrantLock();
+
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
+
+    }
+}
+
+class MyProducer implements Runnable {
+    private final List<String> buffer;
+    private final String color;
+    private final ReentrantLock bufferLock;
+
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    public void run() {
+        Random random = new Random();
+        String[] nums = {"1", "2", "3", "4", "5"};
+        for (String num : nums) {
+            try {
+                System.out.println(color + "Adding..." + num);
+                bufferLock.lock();
+                buffer.add(num);
+                bufferLock.unlock();
+
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException ie) {
+                System.out.println("Producer was interrupted!");
+            }
+        }
+        System.out.println(color + "Adding EOF and exiting...");
+        bufferLock.lock();
+        buffer.add("EOF");
+        bufferLock.unlock();
+    }
+}
+
+class MyConsumer implements Runnable {
+    private final List<String> buffer;
+    private final String color;
+    private final ReentrantLock bufferLock;
+
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    public void run() {
+        while (true) {
+                bufferLock.lock();
+                if (buffer.isEmpty()) {
+                    bufferLock.unlock();
+                    continue;
+                }
+                if (buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Exiting...");
+                    bufferLock.unlock();
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+                bufferLock.unlock();
+        }
+    }
+}
